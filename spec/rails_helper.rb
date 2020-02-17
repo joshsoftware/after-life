ed41f78c8d136b9f5393ddc143aa25ecca7c8a88
Shubham-nagpure <nagpure.sr7@gmail.com>
2020-02-17 11:00:13 +0530

    Bootstrap, font-awesome gem intigrated and erb to haml convertion

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,9 +1,11 @@
+# frozen_string_literal: true
+
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../config/environment', __dir__)
 # Prevent database truncation if the environment is production
-abort("The Rails environment is running in production mode!") if Rails.env.production?
+abort('The Rails environment is running in production mode!') if Rails.env.production?
 require 'rspec/rails'
 require 'database_cleaner'
 # require 'capybara/rspec'
@@ -62,31 +64,30 @@ RSpec.configure do |config|
   # config.filter_gems_from_backtrace("gem name")
 
   config.before(:suite) do
-   DatabaseCleaner.clean_with(:truncation)
+    DatabaseCleaner.clean_with(:truncation)
   end
 
   config.before(:each) do
-   DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.strategy = :transaction
   end
 
-  config.before(:each, :js => true) do
-   DatabaseCleaner.strategy = :truncation
+  config.before(:each, js: true) do
+    DatabaseCleaner.strategy = :truncation
   end
 
   config.before(:each) do
-   DatabaseCleaner.start
+    DatabaseCleaner.start
   end
 
   config.after(:each) do
-   DatabaseCleaner.clean
+    DatabaseCleaner.clean
   end
 
   config.before(:all) do
-   DatabaseCleaner.start
+    DatabaseCleaner.start
   end
 
   config.after(:all) do
-   DatabaseCleaner.clean
+    DatabaseCleaner.clean
   end
-
 end
